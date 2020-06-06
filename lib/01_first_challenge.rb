e568bc96db0f3183547192688dac20448374eb42
Kathleen Brennan <brennank14@gmail.com>
2020-06-06 21:29:08 +0000

    Automatically backed up by Learn

--- lib/01_first_challenge.rb
+++ lib/01_first_challenge.rb
@@ -11,7 +11,7 @@ def first_challenge
       },
       :hero => {
         :name => "Romeo",
-        :age => "15",
+        :age => "15"
       },
       :hero_friends => [{
            :name => "Benvolio",
